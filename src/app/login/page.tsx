c4ea9d1c8a015e27b9daed4b5171f8f1925e5495
Saurabh Basak <saurabhbasak1@gmail.com>
2024-07-06 23:29:02 -0400

    Update login page with form and link styling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,52 @@
+import Link from "next/link";
+
 export default function Login() {
-    return <h1>Login</h1>;
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <div className="grid gap-10">
+                <form className="flex flex-col justify-center gap-10">
+                    <div className="flex flex-col gap-1">
+                        <label htmlFor="email">
+                            Email
+                        </label>
+                        <input
+                            type="text"
+                            id="email"
+                            placeholder="name@email.com"
+                            className="border border-gray-300 rounded-md px-4 py-2 text-black"
+                        />
+                    </div>
+                    <div className="flex flex-col gap-1">
+                        <label htmlFor="password">
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            placeholder="Password"
+                            className="border border-gray-300 rounded-md px-4 py-2 text-black"
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="bg-blue-500 hover:bg-blue-600 text-white rounded-md px-4 py-2"
+                    >
+                        Login
+                    </button>
+                </form>
+                <div className="flex justify-center items-center">
+                    <p className="text-white">
+                        Don&apos;t have an account?
+                        <Link
+                            href="/signup"
+                            className="text-blue-500 hover:text-blue-600"
+                        >
+                            {" "}
+                            Sign up
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
