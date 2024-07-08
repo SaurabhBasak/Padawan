e504da2a62cc65c5117d4cddaa2bfd5bc14f5442
Saurabh Basak <saurabhbasak1@gmail.com>
2024-07-08 00:01:37 -0400

    Updated navbar component in home page

--- src/components/navbar/page.tsx
+++ src/components/navbar/page.tsx
@@ -2,9 +2,14 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <div className="grid grid-flow-col">
-            <Link href="/" className="relative py-2 px-3">Padawan</Link>
-            <Link href="/login" className="justify-self-end py-2 px-3">Login</Link>
+        <div className="grid grid-flow-col font-extrabold font-sans text-xl h-11 items-center">
+            <Link
+                href="/"
+                className="relative left-28 top-2 w-32 h-10 border rounded-3xl flex justify-center items-center"
+            >
+                Padawan
+            </Link>
+            {/* <Link href="/login" className="relative justify-self-end top-2 right-7">Login</Link> */}
         </div>
     );
 }
