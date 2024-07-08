9bbb7f5676c6ee1372ec70a6613ad6803faeba3d
Saurabh Basak <saurabhbasak1@gmail.com>
2024-07-08 00:01:59 -0400

    Updated home page with improved layout and content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
 import Navbar from "@/components/navbar/page";
+import Link from "next/link";
 
 export default function Home() {
     return (
         <div>
             <Navbar />
-            <h1 className="text-4xl font-bold text-center">Hello, World!</h1>
+            <div className="flex flex-col justify-center items-center gap-10">
+                <h1 className="text-4xl font-bold w-2/3 text-center">
+                    Find experienced peers to accelerate your learning
+                </h1>
+                <p className="text-lg w-1/3 text-center">
+                    Padawan is a platform that connects you with experienced
+                    peers who can help you learn new skills and grow in your
+                    career.
+                </p>
+                <Link href="/login" className="bg-white hover:bg-slate-300 text-zinc-900 font-bold py-2 px-4 rounded transition-colors shadow-inner">Get Started</Link>
+            </div>
         </div>
     );
 }
