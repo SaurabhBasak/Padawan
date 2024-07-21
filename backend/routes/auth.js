705bbede3dad5c0e0c2a569ccdd875b5099f8f4a
Saurabh Basak <saurabhbasak1@gmail.com>
2024-07-20 22:21:13 -0400

    Modified user schema

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,13 +5,13 @@ const User = require("../models/user");
 const router = express.Router();
 
 router.post("/signup", async (req, res) => {
-    const { username, email, password } = req.body;
+    const { email, password } = req.body;
     try {
         const user = await User.findOne({ email });
         if (user) return res.status(400).json({ msg: "User already exists" });
 
         const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = new User({ username, email, password: hashedPassword });
+        const newUser = new User({ email, password: hashedPassword });
 
         await newUser.save();
         const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
